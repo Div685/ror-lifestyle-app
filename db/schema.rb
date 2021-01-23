cb5acc4847f3492863de2a9eefd6bace8b6577f5
Div685 <divyeshpatel685@gmail.com>
2021-01-23 18:10:40 +0300

    Add category model
    - migration file
    - category.rb
    - category_spec.rb
    - migrate Category using db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_134313) do
+ActiveRecord::Schema.define(version: 2021_01_23_145508) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "authorId"
     t.string "title"
     t.text "text"
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "priority"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
