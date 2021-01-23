4d6b7537785f375f3872dc7ca9b04ad20f8233ac
Div685 <divyeshpatel685@gmail.com>
2021-01-23 18:22:49 +0300

    Add vote model
    - app/models/vote.rb
    - vote_rspec.rb
    - migration file
    - migrate vote model using db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_145508) do
+ActiveRecord::Schema.define(version: 2021_01_23_152119) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "authorId"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2021_01_23_145508) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "userId"
+    t.integer "articleId"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
