6be3fcf89be5f070af37015b1d0f451f166e41ab
Div685 <divyeshpatel685@gmail.com>
2021-01-27 00:03:38 +0300

    update article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # @category = Category.find_by(id: params[:categoryId]) #check this part if you get the errros
     @article = current_user.articles.build(article_params)
-    # @article.authorId = current_user.id
-    # @article.category_ids = params[:article][:category_ids]
-
-    
 
     respond_to do |format|
       if @article.save
