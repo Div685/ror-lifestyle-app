9fb8e39f9211f54f7fc6b9c5943f1fd4a001320d
Div685 <divyeshpatel685@gmail.com>
2021-01-29 00:53:27 +0300

    Add style to category
    - updated show.html and index.html

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,7 +21,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    @category = Category.includes(articles: :author).find(params[:id])
   end
 
   def destroy
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,10 +1,14 @@
 module VotesHelper
   def vote_article(article)
-    vote = Vote.find_by(article: article, user: current_user)
-    if vote
-      content_tag :span, "#{ current_user.name }, you have aready voted"
+    if current_user
+      vote = Vote.find_by(article: article, user: current_user)
+      if vote
+        content_tag :span, "#{ current_user.name }, you have aready voted"
+      else
+        link_to('Vote', article_votes_path(article_id: article.id), method: :post, class: "btn")
+      end
     else
-      link_to('Vote', article_votes_path(article_id: article.id), method: :post)
+      link_to('Vote', new_session_path, class: "")
     end
   end
 end
