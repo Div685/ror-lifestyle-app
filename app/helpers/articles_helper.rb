8f2149ba8faf3e0d9e556b7396775d5b0b7ab7ff
Div685 <divyeshpatel685@gmail.com>
2021-02-01 17:28:02 +0300

    update images

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -10,4 +10,18 @@ module ArticlesHelper
       content_tag(:span, "Total Votes: #{@article.votes.count}", class: 'm-3')
     end
   end
+
+  def image_article(article)
+    if article.attached?
+      image_tag(url_for(article))
+    else
+      image_tag(url_for('https://vitalplanet.com/wp-content/uploads/84726686_s.jpg'), alt: "img-hero")
+    end
+  end
 end
+
+# <% if @article.img.exists? %>
+#   <img src="<%= url_for(@article.img) %>" alt="img-hero">
+# <% else %>
+#   <img src="https://vitalplanet.com/wp-content/uploads/84726686_s.jpg" alt="img-hero">
+# <% end %>
